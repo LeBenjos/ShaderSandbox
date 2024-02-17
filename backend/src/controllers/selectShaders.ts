1b7cb987c0342694beea854e5897b45245757601
LeBenjos <benjamin.schinkel01@outlook.fr>
2024-02-17 22:31:54 +0100

    feat: develop branch

--- backend/src/controllers/selectShaders.ts
+++ backend/src/controllers/selectShaders.ts
@@ -1,3 +1,5 @@
-export default function selectShaders(): void {
+import { NextFunction, Request, Response } from "express";
+
+export default function selectShaders(req: Request, res: Response, next: NextFunction): void {
 
 }
